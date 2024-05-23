01b9aafb8f6b92fdf9db50ea942dcd5fbab2ba70
boomNDS <boomsixboom@gmail.com>
2024-05-24 02:07:23 +0700

    build: add modules nuxt image robots and vueuse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@pinia/nuxt", "@nuxt/test-utils/module"],
+  modules: [
+    "@nuxt/eslint",
+    "@pinia/nuxt",
+    "@nuxt/test-utils/module",
+    "@vueuse/nuxt",
+    "@nuxt/image",
+    "@nuxtjs/robots",
+  ],
+  robots: {
+    rules: {
+      UserAgent: "*",
+      Disallow: "",
+    },
+  },
 });
