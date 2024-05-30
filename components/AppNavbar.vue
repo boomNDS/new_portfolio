ca7be991b9313145fddd4bc08ee8786fe4728c17
boomNDS <boomsixboom@gmail.com>
2024-05-30 12:16:58 +0700

    feat(nav): set icon and using nuxt-img

--- components/AppNavbar.vue
+++ components/AppNavbar.vue
@@ -1,27 +1,22 @@
 <template>
-  <nav class="ml-30px mt-20px">
+  <nav class="mx-30px mt-20px">
     <div class="container mx-auto flex justify-between items-center">
-      <img
+      <nuxt-img
         duration="120"
         delay="100"
         cursor="pointer"
         class="transition ease-in-out hover:-translate-y-1 hover:scale-105"
         src="/img/logo.svg"
-        placeholder="[50, 25, 75, 5]"
-        format="svg"
+        loading="lazy"
+        sizes="148px 60px"
       />
-      <button
-        class="text-white md:hidden focus:outline-none"
-        @click="toggleMenu"
-      >
-        Menu
-      </button>
+      <button class="md:hidden focus:outline-none">Menu</button>
       <div>
         <ul
           cursor="pointer"
           space="x-[4rem]"
           list="none"
-          class="flex"
+          class="flex w-full"
           font="size-38px regular"
         >
           <li
@@ -50,16 +45,11 @@
           </li>
         </ul>
       </div>
+      <button>TH</button>
     </div>
   </nav>
 </template>
 
-<script setup lang="ts">
-const isOpen = ref(false);
-
-const toggleMenu = () => {
-  isOpen.value = !isOpen.value;
-};
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
