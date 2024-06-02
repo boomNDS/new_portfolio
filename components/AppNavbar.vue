f6e4f3c813839743f86ce5b1d3e4096d46c34e85
boomNDS <boomsixboom@gmail.com>
2024-06-03 01:11:59 +0700

    feat(nav): mock navbar

--- components/AppNavbar.vue
+++ components/AppNavbar.vue
@@ -1,55 +1,75 @@
 <template>
-  <nav class="mx-30px mt-20px">
-    <div class="container mx-auto flex justify-between items-center">
+  <nav class="mx-8 mt-5">
+    <div
+      class="container mx-auto flex min-[1045px]:flex-row flex-col items-end min-[1045px]:items-center justify-between relative"
+    >
       <nuxt-img
+        left="0"
+        scale="hover:105 active:100"
         duration="120"
         delay="100"
         cursor="pointer"
-        class="transition ease-in-out hover:-translate-y-1 hover:scale-105"
+        class="absolute transition ease-in-out hover:-translate-y-1"
         src="/img/logo.svg"
         loading="lazy"
-        sizes="148px 60px"
+        w="9.25rem"
+        h="3.75rem"
+        alt="pachara logo"
       />
-      <button class="md:hidden focus:outline-none">Menu</button>
-      <div>
-        <ul
-          cursor="pointer"
-          space="x-[4rem]"
-          list="none"
-          class="flex w-full"
-          font="size-38px regular"
+      <button
+        scale="active:92"
+        duration="100"
+        text="14px"
+        p="2 1"
+        border="4 #33333"
+        class="min-[1045px]:hidden focus:outline-none rounded ease-in shadow-[4px_4px_0px_0px_rgba(0,0,0,0.9)]"
+        @click="isMenuOpen = !isMenuOpen"
+      >
+        Menu
+      </button>
+      <Transition>
+        <div
+          v-if="isLargeScreen || isMenuOpen"
+          :class="`flex items-center justify-center w-full`"
         >
-          <li
-            duration="120"
-            delay="100"
-            class="transition ease-in-out hover:-translate-y-1 hover:scale-110"
-            p="x-4"
+          <ul
+            cursor="pointer"
+            space="min-[1045px]:x-[3rem]"
+            list="none"
+            class="flex min-[1045px]:flex-row flex-col"
+            font="md:size-38px size-24px regular"
           >
-            <p p="0" m="0">Experience</p>
-          </li>
-          <li
-            duration="120"
-            delay="100"
-            class="transition ease-in-out hover:-translate-y-1 hover:scale-110"
-            p="x-4"
-          >
-            Tech stack
-          </li>
-          <li
-            duration="120"
-            delay="100"
-            class="transition ease-in-out hover:-translate-y-1 hover:scale-110"
-            p="x-4"
-          >
-            Showcase
-          </li>
-        </ul>
-      </div>
-      <button>TH</button>
+            <li
+              v-for="item in menuItems"
+              :key="item"
+              scale="hover:110 active:105"
+              duration="120"
+              delay="100"
+              class="transition ease-in-out hover:-translate-y-1 md:text-left text-center"
+              p="x-4"
+            >
+              <p p="0" m="0">{{ item }}</p>
+            </li>
+          </ul>
+        </div>
+      </Transition>
     </div>
   </nav>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import { useMediaQuery } from "@vueuse/core";
 
-<style scoped></style>
+useHead({
+  title: "pachara's portfolio",
+});
+
+const isMenuOpen = ref(false);
+const menuItems = ["Experience", "Tech stack", "Showcase"];
+const isLargeScreen = useMediaQuery("(min-width: 1045px)");
+</script>
+
+<style scoped>
+/* Add any additional styling if needed */
+</style>
---
+++ components/commons/Dropdown.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <button
+      scale="active:92"
+      duration="100"
+      text="14px"
+      p="2 1"
+      bg="transparent hover:[#e8e8e8]"
+      border="none"
+      class="rounded ease-in"
+      @click="onToggle"
+    >
+      {{ modelValue }}
+      <div
+        :duration="200"
+        :class="[
+          isActive ? `i-mdi-keyboard-arrow-up` : 'i-mdi-keyboard-arrow-down',
+        ]"
+        class="ease-in-out text-xl"
+      />
+    </button>
+    <ul
+      v-show="isActive"
+      duration="100"
+      bg="#e8e8e8"
+      list="none"
+      class="absolute ease-in-out rounded mt-1 m-0 p-0 w-[65px]"
+    >
+      <li
+        v-for="text in list"
+        :key="`li-` + text"
+        bg="hover:[#d9d9d9]"
+        class="rounded px-2 py-1"
+        @click="$emit('on-click', text), onToggle()"
+      >
+        {{ text }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+defineProps<{
+  modelValue: string;
+  list: Array<string>;
+}>();
+
+const isActive = ref<boolean>(false);
+
+const onToggle = () => {
+  isActive.value = !isActive.value;
+};
+</script>
+
+<style scoped></style>
