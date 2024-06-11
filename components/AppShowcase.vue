eb19af73526e6b52c8d7d186f6c2719894dee142
boomNDS <boomsixboom@gmail.com>
2024-06-11 17:52:30 +0700

    feat(showcase): filter item

--- components/AppShowcase.vue
+++ components/AppShowcase.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="mx-auto px-[2rem] lg:px-[5rem] mb-5">
-    <h2 class="title">Showcase</h2>
+  <div class="mx-auto px-[2rem] lg:px-[5rem] mb-8">
+    <div class="flex items-center justify-between">
+      <h2 class="title">Showcase</h2>
+      <CommonsNDropdrop v-model="selected" />
+    </div>
     <section
       class="grid grid-flow-row grid-cols-1 min-[586px]:grid-cols-2 min-[900px]:grid-cols-3 xl:grid-cols-4 gap-8"
     >
       <CommonsInfoCard
-        v-for="(data, index) in jsonData"
+        v-for="(data, index) in items"
         :key="`showcase-${index}`"
         :image-src="data.img"
         :image-alt="`${data.img} image`"
@@ -15,12 +18,37 @@
         :tags="data.tags"
       />
     </section>
+    <div v-if="takeItem < filterItems.length" class="mt-5 flex justify-center">
+      <CommonsButton label="show more" bg="#f2f2f2" @on-click="loadMore" />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref, computed } from "vue";
 import jsonData from "~/public/showcase.json";
 import type { Link } from "./commons/InfoCard.vue";
+
+const takeItem = ref(4);
+const selected = ref("all");
+
+const filterItems = computed(() => {
+  return jsonData.filter((data) => {
+    if (selected.value === "all") return true;
+    if (selected.value === "other") {
+      return !["frontend", "backend", "design"].some((category) =>
+        data.category.includes(category),
+      );
+    }
+    return data.category.includes(selected.value);
+  });
+});
+
+const items = computed(() => filterItems.value.slice(0, takeItem.value));
+
+const loadMore = () => {
+  takeItem.value += 3;
+};
 </script>
 
 <style scoped></style>
---
+++ components/commons/NDropdrop.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <select
+      v-model="localValue"
+      scale="active:92"
+      duration="100"
+      text="14px black"
+      p="2 1"
+      border="4 #333333"
+      bg="#ffc107"
+      class="focus:outline-none rounded ease-in shadow-[4px_4px_0px_0px_rgba(0,0,0,0.9)]"
+    >
+      <option value="all">All</option>
+      <option value="frontend">Frontend</option>
+      <option value="backend">Backend</option>
+      <option value="design">Design</option>
+      <option value="other">Other</option>
+    </select>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  modelValue: {
+    type: String,
+    required: true,
+  },
+});
+const emit = defineEmits(["update:modelValue"]);
+const localValue = ref(props.modelValue);
+watch(localValue, (newValue) => {
+  emit("update:modelValue", newValue);
+});
+</script>
+
+<style scoped></style>
